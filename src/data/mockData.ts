74725d73618dc0bc0d2463972de42eed9eb664d8
Saban-94 <hsaban2025@gmail.com>
2026-05-30 02:23:57 +0300

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -2,7 +2,7 @@ import { Chat, UserProfile, StatusStory } from '../types';
 
 export const currentUserProfile: UserProfile = {
   name: 'הראל סבן',
-  avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&fit=crop&w=150&h=150&q=80',
+  avatar: 'https://i.postimg.cc/J7F9n0c6/Gemini-Generated-Image-9or8fm9or8fm9or8.png',
   status: 'מנהל מערכת ח. סבן',
   phoneNumber: '+972 50-123-4567',
 };
